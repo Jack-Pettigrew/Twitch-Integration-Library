5f37ef703f8bae5bcb1f07a5b1f42f467b68f9a3
Jack Pettigrew <36480371+Jack-Pettigrew@users.noreply.github.com>
2025-12-29 15:54:51 +0000

    [WIP] TwitchWebSocketClient

---
+++ TwitchWebSocketClient.cs
@@ -0,0 +1,42 @@
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json.Nodes;
+
+namespace TIL.Client;
+
+class TwitchWebSocketClient : IDisposable
+{
+    private const string WEBSOCKET_ENDPOINT = "wss://eventsub.wss.twitch.tv/ws";
+
+    public string? sessionId { get; private set; } = null;
+    private ClientWebSocket websocket;
+    private ArraySegment<byte> websocketResponseBuffer = new ArraySegment<byte>(new byte[1024]);
+
+    public TwitchWebSocketClient()
+    {
+        websocket = new ClientWebSocket();
+    }
+
+    public async Task<bool> ConnectToTwitchAsync(CancellationToken cancellationToken)
+    {
+        await websocket.ConnectAsync(new Uri(WEBSOCKET_ENDPOINT), cancellationToken);
+
+        if (websocket.State != WebSocketState.Open)
+        {
+            return false;
+        }
+
+        WebSocketReceiveResult result = await websocket.ReceiveAsync(websocketResponseBuffer, cancellationToken);
+
+        JsonNode responseNode = JsonNode.Parse(Encoding.UTF8.GetString(websocketResponseBuffer.Array!, 0, result.Count))!;
+
+        sessionId = responseNode!["payload"]!["session"]!["id"]?.ToString();
+
+        return true;
+    }
+
+    public void Dispose()
+    {
+        websocket?.Dispose();
+    }
+}
